295918cf753f8f7aa862d8a832a57e31fab7582f
Yoshihide Chubachi <yoshi@chubachi.net>
2011-11-13 02:11:44 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsDeviseOmniauth::Application.config.secret_token = '01400b372791bc5ad4b15e51924952e4d098a6f97872a4df1aa3c01c85a7c22ac0d597af7e5f53d2872508117ea7f21ce42003ee459fcb72616f4eeef6249c2a'
