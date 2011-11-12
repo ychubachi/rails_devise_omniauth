0f209330c1a9c43366d052dd1d82f1fd2dbab515
Yoshihide Chubachi <yoshi@chubachi.net>
2011-11-13 03:54:06 +0900

    Typo at routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ RailsDeviseOmniauth::Application.routes.draw do
   devise_for :users
 
   get "home/index"
-  root :to "home#index"
+  root :to => "home#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
