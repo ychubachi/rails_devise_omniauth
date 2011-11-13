657a51bc15fa526a966a35662131e7f8828bdbd5
Yoshihide Chubachi <yoshi@chubachi.net>
2011-11-13 16:54:29 +0900

    Moved before_fiter from application to home controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 # app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :authenticate_user!
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
   end
-
+  before_filter :authenticate_user!
 end
